630bb72ba79998c8798462044568dadf7e37f3df
Kaveen Hiniduma <hiniduma.1@buckeyemail.osu.edu>
2025-07-04 16:11:06 -0400

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://idtlab.github.io/",
+  url: "https://idtlab.osu.edu",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
