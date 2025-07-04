116017a3c7d4a5b62ec01966b04979ffe4f7a6a4
Kaveen Hiniduma <hiniduma.1@buckeyemail.osu.edu>
2025-07-04 16:16:45 -0400

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   url: "https://idtlab.osu.edu",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/idtlab.github.io/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
